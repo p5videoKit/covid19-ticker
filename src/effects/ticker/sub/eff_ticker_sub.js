cefbbfb6a42c7311521d5a93b0559d1723d593f4
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-16 11:26:36 -0400

    Update eff_ticker_sub.js

--- src/effects/ticker/sub/eff_ticker_sub.js
+++ src/effects/ticker/sub/eff_ticker_sub.js
@@ -3,28 +3,11 @@
 export default class eff_ticker_sub {
   constructor(props) {
     Object.assign(this, props);
-
-    this.width = this.eff_spec.urect.width;
-    this.height = this.eff_spec.urect.height;
-    this.output = createGraphics(width, height);
-    // console.log('width', this.width);
-
-    // this.display_single_date = this.most_lost_ndays != 0;
-
-    // dynamic import - .
-    import('./sub/init.js')
-      .then((module) => {
-        // console.log('eff_ticker module', module);
-        this.init();
-      })
-      .catch((err) => {
-        console.log('eff_ticker err', err);
-      });
   }
   init() {
     console.log('eff_ticker_sub init');
   }
   prepareOutput() {
-    console.log('eff_ticker prepareOutput stub');
+    console.log('eff_ticker_sub prepareOutput stub');
   }
 }
