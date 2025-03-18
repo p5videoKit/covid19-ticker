17a798b23e30a2a3c3f2e4b2583b03d02ab78416
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-18 08:55:55 -0400

    eff_worley

--- src/effects/eff_worley.js
+++ src/effects/eff_worley.js
@@ -10,23 +10,73 @@ export default class eff_worley {
   constructor(props) {
     Object.assign(this, props);
     // console.log('eff_a_example_props props', props);
-    let { width, height } = this.input;
-    this.output = createGraphics(width, height);
+    this.points = [];
+    this.frameIndex = 0;
+    this.frameDelta = 1;
+    this.initGraphics();
+    this.initPoints();
+  }
+  initGraphics() {
+    this.output = createGraphics(100, 100);
     console.log('eff_worley constructor width, height', width, height);
   }
-
+  initPoints() {
+    let points = this.points;
+    let w = 100;
+    let h = 100;
+    for (let i = 0; i < 20; i++) {
+      let x = random(w);
+      let y = random(h);
+      let z = random(w);
+      points[i] = { x, y, z };
+      // points[i] = createVector(random(width), random(height), random(width));
+    }
+  }
   prepareOutput() {
     // console.log('eff_example prepareOutput text_prop', this.text_prop);
     let { output } = this;
-    let { width, height } = output;
-    output.fill(255);
-    {
-      let x = 0 + width * 0.5;
-      let y = height * 0.1;
-      let txsize = height * 0.1;
-      output.textSize(txsize);
-      let txt = this.num_prop;
-      output.text(txt, x, y);
+    this.updateLayer(output);
+  }
+
+  updateLayer(layer) {
+    let points = this.points;
+    layer.loadPixels();
+    let w = layer.width;
+    let h = layer.height;
+    let ww = w * 1;
+    this.frameIndex += this.frameDelta;
+    if (this.frameIndex >= w || this.frameIndex < 0) {
+      this.frameDelta = this.frameDelta * -1;
+      this.frameIndex += this.frameDelta;
     }
+    for (let x = 0; x < w; x++) {
+      for (let y = 0; y < h; y++) {
+        let distances = [];
+        for (let i = 0; i < points.length; i++) {
+          let v = points[i];
+          let z = this.frameIndex;
+          // let z = frameCount % w;
+          let d = dist(x, y, z, v.x, v.y, v.z);
+          distances[i] = d;
+        }
+        let sorted = sort(distances);
+
+        // let r = map(sorted[0], 0, 150, 0, 255);
+        // let g = map(sorted[1], 0, 50, 255, 0);
+        // let b = map(sorted[2], 0, 200, 255, 0);
+
+        // bright heart
+        let r = map(sorted[0], 0, 50, 0, 255);
+        let g = map(sorted[1], 0, 50, 0, 255);
+        let b = map(sorted[2], 0, 50, 0, 255);
+
+        let index = (x + y * w) * 4;
+        layer.pixels[index + 0] = r;
+        layer.pixels[index + 1] = g;
+        layer.pixels[index + 2] = b;
+        layer.pixels[index + 3] = 255;
+      }
+    }
+    layer.updatePixels();
   }
 }
